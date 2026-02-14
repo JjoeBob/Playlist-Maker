8e311dc80517562a7845f0f0410397d041f87f67
JoeBob <e@povedenok.ru>
2026-02-14 12:42:13 +0600

    Navigation, Settings Activity and Intent implementation

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,14 +1,37 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val searchButton = findViewById<Button>(R.id.main_search_button)
+        val libraryButton = findViewById<Button>(R.id.main_library_button)
+        val settingsButton = findViewById<Button>(R.id.main_settings_button)
+
+        searchButton.setOnClickListener {
+            val searchIntent = Intent(this, SearchActivity::class.java)
+            startActivity(searchIntent)
+        }
+
+
+        val libraryButtonOnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                val libraryIntent = Intent(this@MainActivity, LibraryActivity::class.java)
+                startActivity(libraryIntent)
+            }
+        }
+        libraryButton.setOnClickListener(libraryButtonOnClickListener)
+
+        settingsButton.setOnClickListener {
+            val settingsIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingsIntent)
+        }
     }
 }
