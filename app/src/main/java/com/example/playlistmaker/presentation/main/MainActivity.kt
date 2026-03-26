18d1d60eb1c1693ca5bc5fe82d1915c495c9ee10
JoeBob <e@povedenok.ru>
2026-03-26 19:00:28 +0600

    implement search screen track list

--- app/src/main/java/com/example/playlistmaker/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/presentation/main/MainActivity.kt
@@ -1,15 +1,29 @@
-package com.example.playlistmaker
+package com.example.playlistmaker.presentation.main
 
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.playlistmaker.R
+import com.example.playlistmaker.presentation.library.LibraryActivity
+import com.example.playlistmaker.presentation.search.SearchActivity
+import com.example.playlistmaker.presentation.settings.SettingsActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.root)) { view, windowInsets ->
+            val systemBars = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            windowInsets
+        }
 
         val searchButton = findViewById<Button>(R.id.main_search_button)
         val libraryButton = findViewById<Button>(R.id.main_library_button)
