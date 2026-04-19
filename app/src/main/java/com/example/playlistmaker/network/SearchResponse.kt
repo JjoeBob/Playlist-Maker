aec52d0203db37a9f0e768267b2e68adf3d6ca0a
JoeBob <e@povedenok.ru>
2026-04-19 16:49:16 +0600

    implement search history

--- app/src/main/java/com/example/playlistmaker/network/SearchResponse.kt
+++ app/src/main/java/com/example/playlistmaker/network/SearchResponse.kt
@@ -1,14 +1,15 @@
-    package com.example.playlistmaker.network
+package com.example.playlistmaker.network
 
-    import com.google.gson.annotations.SerializedName
+import com.google.gson.annotations.SerializedName
 
-    data class SearchResponse(
-        val results: List<Track>
-    )
+data class SearchResponse(
+    val results: List<Track>
+)
 
-    data class Track(
-        val trackName: String,
-        val artistName: String,
-        @SerializedName("trackTimeMillis") val trackTime: Long,
-        @SerializedName("artworkUrl100") val artworkUrl: String?
-    )
+data class Track(
+    val trackId: Int,
+    val trackName: String,
+    val artistName: String,
+    @SerializedName("trackTimeMillis") val trackTime: Long,
+    @SerializedName("artworkUrl100") val artworkUrl: String?
+)
